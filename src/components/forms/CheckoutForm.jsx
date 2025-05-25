159042a723059ebcef6cdb510e07c2a78616361b
JocRivera <jose_mrivera@soy.sena.edu.co>
2025-05-24 20:02:21 -0500

    icon

--- src/components/forms/CheckoutForm.jsx
+++ src/components/forms/CheckoutForm.jsx
@@ -272,7 +272,7 @@ export function CheckoutForm() {
                                             htmlFor="apple"
                                             className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
                                         >
-                                            <Icons.mercadoPago className="mb-3 h-6 w-6" />
+                                            <Icons.apple className="mb-3 h-6 w-6" />
                                             MercadoPago
                                         </Label>
                                     </div>
